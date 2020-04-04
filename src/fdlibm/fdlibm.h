9fc6c5afc64983fe685da6c1ba833b25a1c81819
jtm <james@mathews.top>
2020-04-04 12:53:38 +0300

    move stdlib include out of namespace
    this causes issues on newer gccs

--- src/fdlibm/fdlibm.h
+++ src/fdlibm/fdlibm.h
@@ -6,7 +6,7 @@
  *
  * Developed at SunPro, a Sun Microsystems, Inc. business.
  * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice 
+ * software is freely granted, provided that this notice
  * is preserved.
  * ====================================================
  */
@@ -17,6 +17,8 @@
 typedef int int32_t;
 typedef unsigned int uint32_t;
 
+#include <stdlib.h>
+
 namespace fdlibm {
 
 /* AIX needs _XOPEN_SOURCE */
@@ -24,7 +26,6 @@ namespace fdlibm {
 #define _XOPEN_SOURCE
 #endif
 
-#include <stdlib.h>
 
 /* GCJ LOCAL: Include files.  */
 #include "ieeefp.h"
@@ -50,12 +51,12 @@ namespace fdlibm {
 #define	HUGE	((float)3.40282346638528860e+38)
 #endif
 
-/* 
+/*
  * set X_TLOSS = pi*2**52, which is possibly defined in <values.h>
  * (one may replace the following line by "#include <values.h>")
  */
 
-#define X_TLOSS		1.41484755040568800000e+16 
+#define X_TLOSS		1.41484755040568800000e+16
 
 /* These typedefs are true for the targets running Java. */
 
@@ -125,13 +126,13 @@ extern double scalb __P((double, double));
 extern double significand __P((double));
 
 /* ieee style elementary functions */
-extern double __ieee754_sqrt __P((double));			
-extern double __ieee754_acos __P((double));			
-extern double __ieee754_acosh __P((double));			
-extern double __ieee754_log __P((double));			
-extern double __ieee754_atanh __P((double));			
-extern double __ieee754_asin __P((double));			
-extern double __ieee754_atan2 __P((double,double));			
+extern double __ieee754_sqrt __P((double));
+extern double __ieee754_acos __P((double));
+extern double __ieee754_acosh __P((double));
+extern double __ieee754_log __P((double));
+extern double __ieee754_atanh __P((double));
+extern double __ieee754_asin __P((double));
+extern double __ieee754_atan2 __P((double,double));
 extern double __ieee754_exp __P((double));
 extern double __ieee754_cosh __P((double));
 extern double __ieee754_fmod __P((double,double));
@@ -181,13 +182,13 @@ extern float rintf __P((float));
 extern double scalbn __P((double, int));
 
 /* ieee style elementary float functions */
-extern float __ieee754_sqrtf __P((float));			
-extern float __ieee754_acosf __P((float));			
-extern float __ieee754_acoshf __P((float));			
-extern float __ieee754_logf __P((float));			
-extern float __ieee754_atanhf __P((float));			
-extern float __ieee754_asinf __P((float));			
-extern float __ieee754_atan2f __P((float,float));			
+extern float __ieee754_sqrtf __P((float));
+extern float __ieee754_acosf __P((float));
+extern float __ieee754_acoshf __P((float));
+extern float __ieee754_logf __P((float));
+extern float __ieee754_atanhf __P((float));
+extern float __ieee754_asinf __P((float));
+extern float __ieee754_atan2f __P((float,float));
 extern float __ieee754_expf __P((float));
 extern float __ieee754_coshf __P((float));
 extern float __ieee754_fmodf __P((float,float));
@@ -239,10 +240,10 @@ extern int   __kernel_rem_pio2f __P((float*,float*,int,int,int,const int32_t*));
 
 #ifdef __IEEE_BIG_ENDIAN
 
-typedef union 
+typedef union
 {
   double value;
-  struct 
+  struct
   {
     uint32_t msw;
     uint32_t lsw;
@@ -253,10 +254,10 @@ typedef union
 
 #ifdef __IEEE_LITTLE_ENDIAN
 
-typedef union 
+typedef union
 {
   double value;
-  struct 
+  struct
   {
     uint32_t lsw;
     uint32_t msw;
